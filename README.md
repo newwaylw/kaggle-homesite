f0a4457573a1b2e2335cb99eec983a7e54a66b1e
Wei Liu <wei@swiftkey.com>
2015-12-22 14:50:46 +0000

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # kaggle-homesite
 project for kaggle homesite
+
+mappings.txt 枚举了所有非numeric的特征与其位置。
+
+normalize_train.py 做了几件事情：
+* 忽略第一个特征"QuoteNumber"
+* 特征 Original_Quote_Date 转换为 "week of the year" [1-52]
+* "QuoteConversion_Flag" 变为最后一个feature
+* 将所有非numeric特征按位置映射为[0,1]的一组特征。
+  比如一个例子的特征是 'XY', 而所有例子有这个特征一共有"XX,XY,XZ"
+  那么这个特征将被变换为 "0 1 0" (XY为1)，以此类推
+
+用法:
+    
+    normailize_train.py mappings.txt train.csv
+    
+test.csv 同样用normailize_test.py处理 （test.csv没有 "QuoteConversion_Flag")
---
+++ normailize_test.py
@@ -0,0 +1,61 @@
+#! /usr/bin/env python3
+
+import sys,re,csv
+from datetime import datetime
+
+def isnumeric(str1):
+  try:
+    float(str1)
+    return True
+  except ValueError:
+    return False
+
+m = dict()
+record = list()
+y = list()
+
+#read mappings
+with open(sys.argv[1]) as f:
+  for line in f:
+    cols = re.split('\s+', line.strip())
+    m[int(cols[0])] = cols[1:]
+'''
+l = sorted(m)
+for i in l:
+  print (i, m[i])
+'''
+with open(sys.argv[2]) as f:
+  header = f.readline()
+  r = csv.reader(f,delimiter=',' ,skipinitialspace=True)
+  for row in r:
+    arow=list()
+    weekofyear = datetime.strptime(row[1], '%Y-%m-%d').date().isocalendar()[1]
+    row[1] = weekofyear
+    row[6] = re.sub(',','',row[7])
+
+    arow.extend(row[1:])
+    record.append(arow)
+
+absent_value = -1
+no =0
+for row in record:
+  idx = 0
+  norm_feature = list()
+  for col in row:
+    if idx in m:
+      l = sorted(m[idx])
+      for item in l:
+        if col == item:
+          norm_feature.append(1)
+        else:
+          norm_feature.append(0)
+
+    elif len(str(col)) == 0 :
+      norm_feature.append(absent_value)
+    else:
+      norm_feature.append(float(col))
+    idx+=1
+  print(' '.join(str(v) for v in norm_feature))
+  no+=1
+
+
